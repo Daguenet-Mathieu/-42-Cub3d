29c03881f5ad226c730e36267837864addf915ff
aurxf <aurxfpro@gmail.com>
2023-12-29 18:12:01 +0100

    textureeee

--- mlx.c
+++ mlx.c
@@ -38,16 +38,16 @@ void	set_hooks_mlx(t_env *env)
 {
 	// void *ptr;
 	// ptr = mlx_xpm_file_to_image(env->mlx.mlx, "pexels-photo-207142.xpm", &x, &y);
-	env->map.texture_no.ptr = mlx_xpm_file_to_image(env->mlx.mlx, env->map.so, &env->map.texture_no.size_line, &env->map.texture_no.height);
+	env->map.texture_no.ptr = mlx_xpm_file_to_image(env->mlx.mlx, env->map.no, &env->map.texture_no.size_line, &env->map.texture_no.height);
 	env->map.texture_so.ptr = mlx_xpm_file_to_image(env->mlx.mlx, env->map.so, &env->map.texture_so.size_line, &env->map.texture_so.height);
 	env->map.texture_we.ptr = mlx_xpm_file_to_image(env->mlx.mlx, env->map.we, &env->map.texture_we.size_line, &env->map.texture_we.height);
 	env->map.texture_ea.ptr = mlx_xpm_file_to_image(env->mlx.mlx, env->map.ea, &env->map.texture_ea.size_line, &env->map.texture_ea.height);
 	printf("no = %p so = %p ea = %p we = %p\n", env->map.texture_no.ptr, env->map.texture_so.ptr, env->map.texture_ea.ptr, env->map.texture_we.ptr);
 	printf("path == %s\n%s\n%s\n%s\n",  env->map.no,  env->map.so,  env->map.ea,  env->map.we);
 	env->map.texture_no.img = (int *)mlx_get_data_addr(env->map.texture_no.ptr, &env->map.texture_no.bpp, &env->map.texture_no.size_line, &env->map.texture_no.endian);
-        env->map.texture_so.img = (int *)mlx_get_data_addr(env->map.texture_so.ptr, &env->map.texture_so.bpp, &env->map.texture_so.size_line, &env->map.texture_so.endian);
-        env->map.texture_we.img = (int *)mlx_get_data_addr(env->map.texture_we.ptr, &env->map.texture_we.bpp, &env->map.texture_we.size_line, &env->map.texture_we.endian);
-        env->map.texture_ea.img = (int *)mlx_get_data_addr(env->map.texture_ea.ptr, &env->map.texture_ea.bpp, &env->map.texture_ea.size_line, &env->map.texture_ea.endian);
+	env->map.texture_so.img = (int *)mlx_get_data_addr(env->map.texture_so.ptr, &env->map.texture_so.bpp, &env->map.texture_so.size_line, &env->map.texture_so.endian);
+	env->map.texture_we.img = (int *)mlx_get_data_addr(env->map.texture_we.ptr, &env->map.texture_we.bpp, &env->map.texture_we.size_line, &env->map.texture_we.endian);
+    env->map.texture_ea.img = (int *)mlx_get_data_addr(env->map.texture_ea.ptr, &env->map.texture_ea.bpp, &env->map.texture_ea.size_line, &env->map.texture_ea.endian);
 	mlx_hook(env->mlx.mlx_win, 17, 0, mlx_close, env);
 	mlx_hook(env->mlx.mlx_win, 3, 1 << 1, handle_keyrelease, env);
 	mlx_hook(env->mlx.mlx_win, 2, 1L << 0, handle_keypress, env);
--- print_wall.c
+++ print_wall.c
@@ -1,5 +1,45 @@
 #include "cub3D.h"
 
+void	init_orientation_indexwall(t_print_wall *s, t_env *env, t_get_next_wall wall)
+{
+	if (wall.x > -1)
+	{
+		s->index_wall = fmod(env->v.new_y, SIZE_CUBE);
+		if ((int)env->map.pixel_x_player > wall.x)
+		{
+			s->orientation = WEST;
+			env->map.img = env->map.texture_we.img;
+			s->ratio_leon = env->map.texture_we.height / wall.height;
+			s->column_texture = s->index_wall * (double)env->map.texture_we.height / (double)SIZE_CUBE;
+		}
+		else
+		{
+			s->orientation = EAST;
+			env->map.img = env->map.texture_ea.img;
+			s->ratio_leon = env->map.texture_ea.height / wall.height;
+			s->column_texture = s->index_wall * (double)env->map.texture_ea.height / (double)SIZE_CUBE;
+		}
+	}
+	if (wall.y > -1)
+	{
+		s->index_wall = fmod(env->h.new_x, SIZE_CUBE);
+		if ((int)env->map.pixel_y_player > wall.y)
+		{
+			s->orientation = NORTH;
+			env->map.img = env->map.texture_no.img;
+			s->ratio_leon = env->map.texture_no.height / wall.height;
+			s->column_texture = s->index_wall * (double)env->map.texture_no.height / (double)SIZE_CUBE;
+		}
+		else
+		{
+			s->orientation = SOUTH;
+			env->map.img = env->map.texture_so.img;
+			s->ratio_leon = env->map.texture_so.height / wall.height;
+			s->column_texture = s->index_wall * (double)env->map.texture_so.height / (double)SIZE_CUBE;
+		}
+	}
+}
+
 int	get_pixel(t_env *env, t_print_wall s, int size_line)
 {
 	double	j;
@@ -12,26 +52,6 @@ int	get_pixel(t_env *env, t_print_wall s, int size_line)
 	j -= tmp;
 	pixel = j + s.column_texture;
 	return (env->map.img[(int)pixel]);
-}
-
-void	init_orientation_indexwall(t_print_wall *s, t_env *env, int x, int y)
-{
-	if (x > -1)
-	{
-		if ((int)env->map.pixel_x_player > x)
-			s->orientation = WEST;
-		else
-			s->orientation = EAST;
-		s->index_wall = fmod(env->v.new_y, SIZE_CUBE);
-	}
-	if (y > -1)
-	{
-		if ((int)env->map.pixel_y_player > y)
-			s->orientation = NORTH;
-		else
-			s->orientation = SOUTH;
-		s->index_wall = fmod(env->h.new_x, SIZE_CUBE);
-	}
 }
 
 void	draw_pixel(t_print_wall s, t_env *env, int i_rayon)
@@ -59,17 +79,17 @@ void	print_wall(t_env *env, t_get_next_wall wall)
 	t_print_wall	s;
 
 	ft_memset(&s, 0, sizeof(t_print_wall));
-	env->map.img = env->map.texture_no.img;
-	init_orientation_indexwall(&s, env, wall.x, wall.y);
+	init_orientation_indexwall(&s, env, wall);
 	s.i = 0;
-	s.ratio_leon = env->map.texture_no.height / wall.height;
-	s.column_texture = s.index_wall * (double)env->map.texture_no.height / (double)SIZE_CUBE;
 	s.height_draw = wall.height;
 	if (s.height_draw > HEIGHT_PLANE)
 	{
 		s.height_draw = HEIGHT_PLANE;
 		s.diff = (wall.height - s.height_draw) / (wall.height / s.height_draw * 2);
 	}
+	//printf("height = %f\n", wall.height);
+	//printf("height_draw = %f\n", s.height_draw);
+	//printf("diff = %f\n", s.diff);
 	s.y_start = (HEIGHT_PLANE / 2) - (s.height_draw / 2);
 	draw_pixel(s, env, wall.i_rayon);
 }
